74fe25210be1bac54de42b27fe8f5bfd6eb51721
Илья Манютин <chem-ixtrems@mail.ru>
2022-07-23 20:14:42 +0300

    Тестовый консольный проект

---
+++ Test/Methods_DB_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods_DB_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
