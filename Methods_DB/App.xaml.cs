98aac1278b74c8a281e228178d8b56d6c381578a
Илья Манютин <chem-ixtrems@mail.ru>
2022-07-24 21:23:53 +0300

    Инфрастуктура папок приложения

--- Methods_DB/App.xaml.cs
+++ Methods_DB/App.xaml.cs
@@ -8,10 +8,7 @@ using System.Windows;
 
 namespace Methods_DB
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+      public partial class App : Application
     {
     }
 }
--- Tests/Methods_DB_Test/App.xaml.cs
+++ Tests/Methods_DB_Test/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace Methods_DB_Test
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
