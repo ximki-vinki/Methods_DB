f52739d573c5bd30e9e9a0f0ec2330d4a9a799b9
Илья Манютин <chem-ixtrems@mail.ru>
2022-07-27 16:49:47 +0300

    В модели определил диапазон измерения и ед. измерения

---
+++ Methods_DB/Models/Method.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_DB.Models
+{
+    internal class Method
+    {
+        public string Name { get; set; }
+
+
+    }
+
+    #region Диапазон измерения методики и ед. измерения
+    /// <summary>Диапазон методики</summary>
+    internal struct Range
+    {
+        SubRanges FirstRange { get; set; }
+        SubRanges LastRange { get; set; }
+    }
+    /// <summary>Один из диапазонов методики</summary>
+    internal struct SubRanges
+    {
+        public int NumSubRange { get; set; }
+        public bool IncluedNum { get; set; }
+        public int СoeffSubRange { get; set; }
+        public IEnumerable<UnitOfMeasurement> SubRangeUoM { get; set; }
+    }
+    /// <summary>Еденица измерений</summary>
+    internal struct UnitOfMeasurement
+    {
+        public string NameUoM { get; set; }
+        public int СoeffUoM { get; set; }
+    } 
+    #endregion
+
+}
